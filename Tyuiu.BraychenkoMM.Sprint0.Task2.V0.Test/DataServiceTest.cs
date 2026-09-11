9478418c5ed51056504cccd591550b9275e402bc
BraychenkoMaxim <Tyuiu.BraychenkoMM@outlook.com>
2026-09-11 19:22:04 +0500

    Task2

---
+++ Tyuiu.BraychenkoMM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.BraychenkoMM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BraychenkoMM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            string name = "Максим";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
---
+++ Tyuiu.BraychenkoMM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tyuiu.BraychenkoMM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BraychenkoMM.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+        }
+    }
+}
